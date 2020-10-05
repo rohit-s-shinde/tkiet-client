22873887493084fa5313b7fcb039646599c147d4
mdrizwanansari <54395198+mdrizwanansari@users.noreply.github.com>
2020-10-05 17:57:25 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+// change
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
